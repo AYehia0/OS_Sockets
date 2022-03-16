0491d3e5e8f151e9b8ae21481aa2b16471e6ed4f
AYehia0 <ahmedyehia611@gmail.com>
2022-03-16 09:03:41 +0200

    Removed un-used imports

--- socket_programming/multi-client-server/server.java
+++ socket_programming/multi-client-server/server.java
@@ -1,7 +1,4 @@
-import java.awt.List;
 import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
@@ -9,8 +6,6 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Scanner;
 
 public class MainServer {
 
@@ -71,7 +66,6 @@ public class ClientHandler implements Runnable{
   private BufferedReader inp;
   private PrintWriter out;
   private final String EXIT_STR = "exit";
-  public Scanner clientSc = new Scanner(System.in);
 
   public ClientHandler(Socket socket, String name, BufferedReader inp, PrintWriter out) {
     this.clientsocket = socket;
